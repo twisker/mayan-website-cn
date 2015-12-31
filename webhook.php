5890fe02f58dabe7b7bd2f8d55f040c7e482c702
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-12-31 04:05:43 -0400

    Smarter webhook

--- webhook.php
+++ webhook.php
@@ -1,9 +1,11 @@
 <?php
 $ini_array = parse_ini_file("webhook.ini");
 
+$hugo_command_line = sprintf("cd %s && %s --theme=mayan -s website -d %s", $ini_array["repository_path"], $ini_array["hugo_path"], $ini_array["destination"]);
+$git_command_line = sprintf("cd %s && git pull", $ini_array["repository_path"]);
+
 if ($ini_array["secret"] == $_POST["secret"]) {
-    exec("git pull");
-    $hugo_command_line = sprintf("hugo --theme=mayan -s website -d %s", $ini_array['destination']);
+    exec($git_command_line);
     exec($hugo_command_line);
 }
 ?>
