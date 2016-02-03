8148f98590f92349c7f635f5857cbfb2fa4eb98b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-02-03 16:19:14 -0400

    Update webhook util.

--- utils/webhook.php
+++ utils/webhook.php
@@ -1,7 +1,7 @@
 <?php
 $ini_array = parse_ini_file("webhook.ini");
 
-$hugo_command_line = sprintf("cd %s && %s --theme=mayan -s website -d %s", $ini_array["repository_path"], $ini_array["hugo_path"], $ini_array["destination"]);
+$hugo_command_line = sprintf("cd %s && %s -d %s", $ini_array["repository_path"], $ini_array["hugo_path"], $ini_array["destination"]);
 $git_command_line = sprintf("cd %s && git pull", $ini_array["repository_path"]);
 
 exec($git_command_line);
